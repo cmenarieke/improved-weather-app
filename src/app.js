bf8629a85888703c459e767096548ceb630d8b13
cmenarieke <cmenarieke@gmail.com>
2023-07-05 16:26:34 -1000

    Updated JS to include date and time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
+function showDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let weekday = days[date.getDay()];
+  return `Last updated on ${weekday} at ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
   console.log(response.data);
-
   let cityName = document.querySelector(".city-name");
   cityName.innerHTML = response.data.city;
   let currentTemp = document.querySelector(".current-temp");
@@ -11,6 +36,8 @@ function displayWeather(response) {
   humidity.innerHTML = response.data.temperature.humidity;
   let windspeed = document.querySelector(".windspeed");
   windspeed.innerHTML = Math.round(response.data.wind.speed);
+  let lastUpdated = document.querySelector(".todays-date-time");
+  lastUpdated.innerHTML = showDate(response.data.time * 1000);
 }
 
 let apiKey = "0cb9ed087t764d66183dfo493b5aadf0";
